d4f8ea5f4c8df2d20e24f357220b36ed7436ea66
robertdurst <rsdurst@colby.edu>
2017-09-16 15:02:09 -0700

    init commit

--- MemoSpace/ViewController.swift
+++ MemoSpace/ViewController.swift
@@ -10,13 +10,43 @@ import UIKit
 import SceneKit
 import ARKit
 
-//Hello ROB Hello Obi
-//Please work
-//Hello ROB Hello
+struct MemoImage {
+    let image: UIImage
+    let x: Float
+    let y: Float
+    let z: Float
+}
 
-//Last try
-//I'm really cool
+extension UIImage {
+    func image(withRotation radians: CGFloat) -> UIImage {
+        let cgImage = self.cgImage!
+        let LARGEST_SIZE = CGFloat(max(self.size.width, self.size.height))
+        let context = CGContext.init(data: nil, width:Int(LARGEST_SIZE), height:Int(LARGEST_SIZE), bitsPerComponent: cgImage.bitsPerComponent, bytesPerRow: 0, space: cgImage.colorSpace!, bitmapInfo: cgImage.bitmapInfo.rawValue)!
+        
+        var drawRect = CGRect.zero
+        drawRect.size = self.size
+        let drawOrigin = CGPoint(x: (LARGEST_SIZE - self.size.width) * 0.5,y: (LARGEST_SIZE - self.size.height) * 0.5)
+        drawRect.origin = drawOrigin
+        var tf = CGAffineTransform.identity
+        tf = tf.translatedBy(x: LARGEST_SIZE * 0.5, y: LARGEST_SIZE * 0.5)
+        tf = tf.rotated(by: CGFloat(radians))
+        tf = tf.translatedBy(x: LARGEST_SIZE * -0.5, y: LARGEST_SIZE * -0.5)
+        context.concatenate(tf)
+        context.draw(cgImage, in: drawRect)
+        var rotatedImage = context.makeImage()!
+        
+        drawRect = drawRect.applying(tf)
+        
+        rotatedImage = rotatedImage.cropping(to: drawRect)!
+        let resultImage = UIImage(cgImage: rotatedImage)
+        return resultImage
+        
+        
+    }
+}
+
 class ViewController: UIViewController, ARSCNViewDelegate {
+    
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -29,11 +59,58 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        //Add tap gesture and connect it to a function
+        let tapGesture = UITapGestureRecognizer(target: self, action:
+            #selector(ViewController.handleTap(gestureRecognize:)))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    //The function for the tap
+    @objc
+    func handleTap(gestureRecognize: UITapGestureRecognizer){
+        //Captures the snapshot
+        var imageSnapped = sceneView.snapshot()
+        
+        //Create a new MemoImage
+        var newMemoImage = MemoImage(image: imageSnapped, x: 0, y: 0, z: -0.2)
+        
+        addImage(memoImage: newMemoImage)
+    }
+    
+    //The function to add the image
+    func addImage(memoImage: MemoImage){
+        
+        //Getting the current fram
+        guard let currentFrame = sceneView.session.currentFrame else {
+            return
+        }
+        
+        //Setting up the image plane
+        var imagePlane = SCNPlane(width: sceneView.bounds.width / 6000,
+                                  height: sceneView.bounds.height / 6000)
+        
+        
+        
+        
+        //var imageSnapped = UIImage(named:imageName)!
+        
+        //Adding the image to the imagePlane
+        imagePlane.firstMaterial?.diffuse.contents = memoImage.image
+        imagePlane.firstMaterial?.lightingModel = .constant
+        
+        
+        
+        // Create plane node and add it it the scene
+        let planeNode = SCNNode(geometry: imagePlane)
+        sceneView.scene.rootNode.addChildNode(planeNode)
+        
+        //Transform the image
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = memoImage.z
+        translation.columns.3.x = memoImage.x
+        translation.columns.3.y = memoImage.y
+        planeNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -45,6 +122,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Run the view's session
         sceneView.session.run(configuration)
     }
+    
+    
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
